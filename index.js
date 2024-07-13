28b92025c3e15f9f34e46906cbec13c66ee86b5c
Ishaan1103 <111432439+Ishaan1103@users.noreply.github.com>
2024-07-13 19:08:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import Note from './models/note.js'
-import 'dotenv/config'
 import { Query } from 'mongoose';
 const app = express();
 app.use(express.static('dist'))
